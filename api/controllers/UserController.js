e0a3f7c0b8149c1b7e9ed73440bf6d96b25c29a9
okolilemuel <lemuel@karixchange.com>
2018-01-09 11:10:39 +0000

    changes to user endpoints

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -116,17 +116,9 @@ module.exports = {
      */
     create: function(req, res) {
 
-<<<<<<< HEAD
         if (req.body.password !== req.body.confirmPassword) {
             return res.json(401, { status: 'error', err: 'Passwords doesn\'t match, What a shame!' });
         }
-=======
-        console.log(req.param(req.body));
-
-        // if (req.body.password !== req.body.confirmPassword) {
-        //     return res.json(401, { status: 'error', err: 'Password doesn\'t match, What a shame!' });
-        // }
->>>>>>> 07ec470eefa42b6fb03bc1c0e1e6382c2ecab430
 
         // remove the confirmPassword element from the body object before saving to DB
         delete req.body.confirmPassword;
