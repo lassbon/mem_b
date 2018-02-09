e842a94f8526f8501aaf65b75db0e11fa8ac6557
Lemuel Okoli <lemuel@karixchange.com>
2018-02-09 19:22:28 +0000

    made changes to the alert service

--- api/services/alert.js
+++ api/services/alert.js
@@ -87,40 +87,43 @@ module.exports.referee = function(companyName, userId, email) {
 			sails.log.error(err);
 		}
 
-		// Send action email to the users apointed referees
-		var emailData = {
-			'email': process.env.SITE_EMAIL,
-			'from': process.env.SITE_NAME,
-			'subject': 'Action required on ' + process.env.SITE_NAME + ' membership registration for ' + companyName,
+		if (!referee) {
+			return res.json(404, { status: 'error', err: 'No referee with such id existing' });
+		} else {
+			// Send action email to the users apointed referees
+			var emailData = {
+				'email': process.env.SITE_EMAIL,
+				'from': process.env.SITE_NAME,
+				'subject': 'Action required on ' + process.env.SITE_NAME + ' membership registration for ' + companyName,
 
-			'body': 'Hello!<br><br>' +
-				companyName + ' Appointed you as referee to it\'s registration on the ' + process.env.SITE_NAME + ' membership plartform.<br><br>' +
-				'Click on the appropriate button to CONFIRM or REJECT the applicant for membership.<br><br>' +
-				'<a href=" ' + process.env.VERIFIER_CONFIRM + userId + '/' + referee.id + ' " style="color: green;">CONFIRM</a>.<br><br>' +
-				'<a href=" ' + process.env.VERIFIER_REJECT + userId + '/' + referee.id + ' " style="color: red;">REJECT</a>.<br><br>' +
-				'Thank you for your time.<br><br>' +
-				process.env.SITE_NAME,
+				'body': 'Hello!<br><br>' +
+					companyName + ' Appointed you as referee to it\'s registration on the ' + process.env.SITE_NAME + ' membership plartform.<br><br>' +
+					'Click on the appropriate button to CONFIRM or REJECT the applicant for membership.<br><br>' +
+					'<a href=" ' + process.env.VERIFIER_CONFIRM + userId + '/' + referee.id + ' " style="color: green;">CONFIRM</a>.<br><br>' +
+					'<a href=" ' + process.env.VERIFIER_REJECT + userId + '/' + referee.id + ' " style="color: red;">REJECT</a>.<br><br>' +
+					'Thank you for your time.<br><br>' +
+					process.env.SITE_NAME,
 
-			'to': email
+				'to': email
+			}
+
+			azureEmail.send(emailData, function(resp) {
+				if (resp === 'success') {
+					sails.log.info('The email was sent successfully.');
+				}
+
+				if (resp === 'error') {
+					sails.log.error(resp);
+				}
+			});
 		}
-
-		azureEmail.send(emailData, function(resp) {
-			if (resp === 'success') {
-				sails.log.info('The email was sent successfully.');
-			}
-
-			if (resp === 'error') {
-				sails.log.error(resp);
-			}
-		});
-
 	});
 };
 
 // Alert the user on referee rejection
 module.exports.rejected = function(res, companyName, email, referee, whoRejected) {
 
-	var rejectionMessage = 'One of your referee ' + referee + ' has rejected your registration.<br> please provide a new and valid referee through this <a href="'+ process.env.REFEREE_LINK + '/' + whoRejected +'" target="blank">LINK</a>.';
+	var rejectionMessage = 'One of your referee ' + referee + ' has rejected your registration.<br> please provide a new and valid referee through this <a href="' + process.env.REFEREE_LINK + '/' + whoRejected + '" target="blank">LINK</a>.';
 
 	// Send email to the user alerting him/her to the state of affairs
 	var emailData = {
