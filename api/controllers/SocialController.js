05e3ecc6a39d775460995123fa1fa6d6cfb7b754
okoli <lemuel@karixchange.com>
2018-02-18 13:14:04 +0000

    made some minor changes

--- api/controllers/SocialController.js
+++ api/controllers/SocialController.js
@@ -518,49 +518,38 @@ module.exports = {
                     return res.json(err.status, { err: err });
                 }
 
-                console.log(requests);
+                // console.log(requests);
 
-                for (let i = 0; i < requestes.length; i++) {
-                    User.findOne({ id: requests[i].requester }).exec(function(err, user) {
+                // for (let i = 0; i < requestes.length; i++) {
+                //     User.findOne({ id: requests[i].requester }).exec(function(err, user) {
+                //         if (err) {
+                //             sails.log.error(err);
+                //             return res.json(err.status, { err: err });
+                //         }
+
+                //         delete user.password;
+                //         requests[i].requester = user;
+                //     });
+                // }
+
+                // return res.json(200, requests);
+
+                Promise.all(requests.map(function(request) {
+                    return User.findOne({ id: request.requester }).then(function(user, err) {
                         if (err) {
                             sails.log.error(err);
-                            return res.json(err.status, { err: err });
                         }
 
                         delete user.password;
-                        requests[i].requester = user;
-                    });
-                }
-
-                // for(var x in requests){
-                //     User.findOne({ id: requests[x].requester }).exec(function(err, user) {
-                //         if (err) {
-                //             sails.log.error(err);
-                //             return res.json(err.status, { err: err });
-                //         }
-
-                //         delete user.password;
-                //         requests[x].requester = user;
-                //     });
-                // };
-
-                // requests.forEach(function(request){
-                //     User.findOne({ id: request.requester }).exec(function(err, user) {
-                //         if (err) {
-                //             sails.log.error(err);
-                //             return res.json(err.status, { err: err });
-                //         }
-
-                //         delete user.password;
-                //         requests.requester = user;
-                //     });
-                // });
-
-                return res.json(200, requests);
+                        request.requester = user
+                        return request
+                    })
+                })).then(function(requests) {
+                    return res.json(200, requests)
+                })
             })
             .catch(function(err) {
                 sails.log.error(err);
-                console.log(err);
                 return res.json(500, { err: err });
             });
     },
